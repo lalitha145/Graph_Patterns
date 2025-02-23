180e23a9ce848e17f7a7ab86095a4f174cd1bb6d
Chintha Lalitha <97296095+lalitha145@users.noreply.github.com>
2025-02-23 19:32:06 +0530

    Time: 11 ms (18.56%) | Memory: 45.5 MB (7.93%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        HashMap<Integer, HashMap<Integer, Integer>> map = buildGraph(flights);
+        
+        pq.add(new int[]{src, 0, 0}); 
+        Map<Integer, Integer> minStops = new HashMap<>(); 
+        while (!pq.isEmpty()) {
+            int[] poll = pq.poll();
+            int node = poll[0];
+            int cost = poll[1];
+            int stops = poll[2];
+            if (node == dst) return cost;
+            
+            if (stops > k) continue;
+            if (minStops.containsKey(node) && minStops.get(node) < stops) continue;
+            minStops.put(node, stops);
+
+            if (!map.containsKey(node)) continue;
+            for (Map.Entry<Integer, Integer> entry : map.get(node).entrySet()) {
+                int adjNode = entry.getKey();
+                int adjCost = entry.getValue();
+                
+                pq.add(new int[]{adjNode, cost + adjCost, stops + 1});
+            }
+        }
+        return -1;
+    }
+
+    private HashMap<Integer, HashMap<Integer, Integer>> buildGraph(int[][] flights) {
+        HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
+        for (int[] flight : flights) {
+            map.putIfAbsent(flight[0], new HashMap<>());
+            map.get(flight[0]).put(flight[1], flight[2]);
+        }
+        return map;
+    }
+}
